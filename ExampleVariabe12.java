67ae685b6baacdd34902ce7fe243d61fe2ddc3ce
hemoabdullah <hamamxklb@gmail.com>
2023-09-15 13:04:53 +0700

    hi

---
+++ ExampleVariabe12.java
@@ -0,0 +1,15 @@
+public class ExampleVariabe12 {
+    public static void main(String[] args) {
+        String oneofmyhoobies = "playing futsal";
+        boolean issmart = true;
+        char gender = 'M' ;
+        byte _age = 20;
+        double $gpa = 3.38, height = 1.68;
+        System.out.println("Oneofmyhoobies " + oneofmyhoobies);
+        System.out.println("Are you smart " + issmart);
+        System.out.println("gender :"+gender);
+        System.out.println("my current age is "+ _age);
+        System.out.printf("My current age is my gpa is %s and my height is %s metres", $gpa, height);
+    }
+    
+}
---
+++ Triangle12.java
@@ -0,0 +1,9 @@
+import java.util.scanner; 
+public class Triangle12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int base, height;
+        float area;
+        System.out.print( "Input base"));
+    }
+}
