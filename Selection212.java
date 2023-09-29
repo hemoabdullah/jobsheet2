ff2a7c15934e48692c2a8d1dfd3c0ce6f35e0f13
hemoabdullah <hamamxklb@gmail.com>
2023-09-29 14:14:59 +0700

    tes

--- Selection212.java
+++ Selection212.java
@@ -11,7 +11,28 @@ public class Selection212 {
         System.out.print( "Nilai tugas  : ");
         float assignment = input212.nextFloat();
         float total = (finalExam * 0.4F) + (midExam * 0.3F) + (quiz * 0.1F) + (assignment * 0.2F);
-        String message = total < 65 ? "Retake" : "pass";
-        System.out.println("Final grade = " + total + " and the decission is " + message);
+        /*String message = total < 65 ? "Retake" : "pass";
+        System.out.println("Final grade = " + total + " and the decission is " + message);*/
+        if(total > 80 && total <= 100){
+            System.out.println("Final grade = A");
+
+        }else if (total > 73 && total <= 80 ){
+        System.out.println("Final grade = B+");
+
+        }else if (total > 65 && total <= 73 ){
+        System.out.println("final grade = B");
+
+        }else if (total > 60 && total <= 65 ){
+        System.out.println("final grade = c+");
+
+        }else if (total > 50 && total <= 60 ){
+        System.out.println("final grade = c");
+
+        }else if (total > 39 && total <= 50 ){
+        System.out.println("final grade = D");
+
+        }else{
+        System.out.println("Final grade = E");
+        }
+        }
     }
-}
