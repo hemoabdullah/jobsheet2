f6eb5751a66c422132a088edd56a5f70aa01c813
hemoabdullah <hamamxklb@gmail.com>
2023-11-15 09:49:18 +0700

    j s 10

---
+++ arraynillay12.java
@@ -0,0 +1,16 @@
+    import java.util.Scanner;
+    public class arraynillay12 {
+public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+int [] bil = new int [4];
+bil[0]= 5;
+bil[1]= 13;
+bil[2]=-7;
+bil[3]= 17;
+for (int i = 0 ;i<4;i++){
+    System.out.println(bil[i]);
+}
+
+    }
+}
+
---
+++ cinemawithscanner12.java
@@ -0,0 +1,3 @@
+public class cinemawithscanner12 {
+    
+}
