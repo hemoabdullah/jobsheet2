d2aab4c30dc9487ad9f5c86087f656600c8b2346
hemoabdullah <hamamxklb@gmail.com>
2023-11-17 13:37:29 +0700

    commit

--- nestedloop12.java
+++ nestedloop12.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
 
 public class nestedloop12 {
-      public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double[][] temps = new double[5][7];
 
-         for (int i = 0; i < temps.length; i++){
+        for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota ke-" + i);
-            for (int j = 0; j < temps[0].length; j++){
+            for (int j = 0; j < temps[0].length; j++) {
                 System.out.print("Hari ke-" + (j + 1) + ": ");
-                temps [i][j] = scanner.nextDouble();
+                temps[i][j] = scanner.nextDouble();
             }
             System.out.println();
         }
-
-        for (int i = 0; i < temps.length; i++){
-            System.out.print("Kota ke-" + (i + 1) + ": ");
-            for (int j = 0; j < temps[0].length; j++){
-                System.out.print(temps[i][j] + " ");
+        for (double[] cityTemps : temps) {
+            for (double temp : cityTemps) {
+                System.out.print(temp + " ");
             }
             System.out.println();
         }
